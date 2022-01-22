99706460d1eade8bf3918ec27d6efb6149c4e2be
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2022-01-22 17:15:37 +0530

    Added : checked valid contacts

--- CreateContact.js
+++ CreateContact.js
@@ -23,23 +23,76 @@ class CreateContact {
 let createContactArr = new Array();
 
 const prompts = require("prompt-sync")();
-
+// for first name
 let firstName = prompts("First name: ");
-console.log(firstName);
+let firstNameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+if (firstNameRegex.test(firstName.value)) {
+    console.log("First name is correct: " + firstName);
+} else {
+    console.log("First name is not correct: ");
+}
+
+// for last name
 let lastName = prompts("Last name: ");
-console.log(lastName);
+let lastNameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+if (lastNameRegex.test(lastName.value)) {
+    console.log("Last name is correct: " + lastName);
+} else {
+    console.log("Last name is not correct: ");
+}
+// For address
 let address = prompts("Address: ");
-console.log(address);
+let addressRegex = RegExp('^[A-Za-z]{4,}$');
+if (addressRegex.test(address.value)) {
+    console.log("Address is correct: " + address);
+} else {
+    console.log("Entered address is not correct: ");
+}
+
+// For city
 let city = prompts("City: ");
-console.log(city);
+let cityRegex = RegExp('^[A-Za-z]{4,}$');
+if (cityRegex.test(address.value)) {
+    console.log("City is correct: " + city);
+} else {
+    console.log("Sorry entered city is not correct");
+}
+
+//for state
 let state = prompts("State: ");
-console.log(state);
+let stateRegex = RegExp('^[A-Za-z]{4,}$');
+if (stateRegex.test(state.value)) {
+    console.log("State is correct: " + state);
+} else {
+    console.log("Sorry entered state is not correct");
+}
+
+//for zip
 let zip = prompts("Zip: ");
-console.log(zip);
+let zipRegex = RegExp('^[0-9]{6,}$');
+if (zipRegex.test(zip.value)) {
+    console.log("Zip is correct: " + zip);
+} else {
+    console.log("Sorry entered zip is not correct");
+}
+
+//for phone number
 let phoneNumber = prompts("Phone number: ");
-console.log(phoneNumber);
+let phoneNumberRegex = RegExp('^([0-9]{2}\\s)?[0-9]{10}$');
+if (phoneNumberRegex.test(phoneNumber.value)) {
+    console.log("Phone number is correct: " + phoneNumber);
+} else {
+    console.log("Sorry entered phone number is not correct");
+}
+
+//for email
 let email = prompts("Email: ");
-console.log(email);
+let emailRegex = RegExp('^[a-z.+_-]+[.a-z0-9]*@[a-z0-9]+[.a-z]+[.a-z0-9]*$');
+if (emailRegex.test(email.value)) {
+    console.log("Email is correct: " + email);
+} else {
+    console.log("Sorry entered email is not correct");
+}
 
 createContactArr.push(new CreateContact(firstName, lastName, address, city, state, zip, phoneNumber, email));
 console.log(createContactArr);
